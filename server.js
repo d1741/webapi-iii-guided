eb397a6c29672f40f4ae1bf33d014d74284ecc27
d1741 <traciecodes@gmail.com>
2019-09-23 12:35:32 -0500

    Installed helmet

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+//Considered GLOBAL MIDDLEWARE: must be at the top of the page to work properly
 // built-in middleware parses the body for us
 server.use(express.json());
 
 //third-party middleware
 //similar to built-in in that someone else wrote it for us and we can just plug it in where we need it
+server.use(helmet());
 
 //router
 server.use('/api/hubs', hubsRouter);
